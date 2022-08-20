1cd3e3a8c12dc14739248e04d0c54ae7a6db7548
Maick Speck <maicksantos05@hotmail.com>
2022-08-20 10:59:11 -0300

    validation post

--- src/domain/service/post.service.ts
+++ src/domain/service/post.service.ts
@@ -1,9 +1,15 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+    BadRequestException,
+    Injectable,
+    NotFoundException
+} from '@nestjs/common';
 import { PostRepository } from 'src/adapter/repository/post.repository';
 import { PostEntity } from '../entity/post.entity';
 
 @Injectable()
 export class PostService {
+    private validationFields = ['title', 'body'];
+
     constructor(private readonly postRepository: PostRepository) {}
 
     getPostById(id: number): PostEntity {
@@ -11,10 +17,30 @@ export class PostService {
             return post.id == id;
         });
 
-        if (postById.length === 0) {
+        this.validateOutput(id, postById);
+
+        return postById[0];
+    }
+
+    validateOutput(id: number, output: PostEntity[]) {
+        if (output.length === 0) {
             throw new NotFoundException(`Post ${id} not found!`);
         }
 
-        return postById[0];
+        this.validationFields.forEach((field: string) => {
+            this.validateField(field, output[0]);
+        });
+    }
+
+    validateField(field: string, output) {
+        if (!Object.keys(output).includes(field)) {
+            throw new BadRequestException(
+                `The requested Post doesn't have the property ${field}`
+            );
+        }
+
+        if (output[field].length === 0 || output[field].length == null) {
+            throw new BadRequestException(`Empty ${field} for requested Post`);
+        }
     }
 }
